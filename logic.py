e3b51c091e5289cbb45f8024ba61177b253e94d6
Farshid <126648034+farshid-cloudly@users.noreply.github.com>
2023-04-09 16:02:50 +0600

    Update logic.py

--- logic.py
+++ logic.py
@@ -116,34 +116,34 @@ class Multiply(Resource):
         return jsonify(ret_map)
 
 
-class Divide(Resource):
-    def post(self):
-        # If I am here, then the resource Divide was requested using the method POST
+# class Divide(Resource):
+#     def post(self):
+#         # If I am here, then the resource Divide was requested using the method POST
 
-        # Step 1: Get posted data:
-        posted_data = request.get_json()
+#         # Step 1: Get posted data:
+#         posted_data = request.get_json()
 
-        # Step 1b: Verify validity of posted data
-        status_code = check_posted_data(posted_data, "division")
+#         # Step 1b: Verify validity of posted data
+#         status_code = check_posted_data(posted_data, "division")
 
-        if status_code != 200:
-            ret_json = {
-                "Message": "An error happened",
-                "Status Code": status_code
-            }
-            return jsonify(ret_json)
+#         if status_code != 200:
+#             ret_json = {
+#                 "Message": "An error happened",
+#                 "Status Code": status_code
+#             }
+#             return jsonify(ret_json)
 
-        # If i am here, then status_code == 200
-        x = posted_data["x"]
-        y = posted_data["y"]
-        x = int(x)
-        y = int(y)
+#         # If i am here, then status_code == 200
+#         x = posted_data["x"]
+#         y = posted_data["y"]
+#         x = int(x)
+#         y = int(y)
 
-        # Step 2: Multiply the posted data
-        ret = (x*1.0)/y
-        ret_map = {
-            'Message': ret,
-            'Status Code': 200
-        }
-        return jsonify(ret_map)
+#         # Step 2: Multiply the posted data
+#         ret = (x*1.0)/y
+#         ret_map = {
+#             'Message': ret,
+#             'Status Code': 200
+#         }
+#         return jsonify(ret_map)
 
